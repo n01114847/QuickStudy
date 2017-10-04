4bae84ab8e42384da472f7bdf50901d6febe3a8a
Ruel John Cootauco <n01114847@humbermail.ca>
2017-10-04 18:30:24 -0400

    QuickStudy module added
    The first version of the prototype app.

---
+++ Source Code/app/src/main/java/cengboiz/quickstudy/MainActivity.java
@@ -0,0 +1,39 @@
+package cengboiz.quickstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.app_info:
+                break;
+            case R.id.app_contact:
+                break;
+            case R.id.app_help:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
